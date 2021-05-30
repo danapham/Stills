9a04939ab0ca2e5352f3981d1631f18cd99735f9
Dana Pham <danapham12@gmail.com>
2021-05-29 19:46:58 -0500

    filled out all the models

--- DataAccess/UsersRepository.cs
+++ DataAccess/UsersRepository.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Stills.DataAccess
 {
     public class UsersRepository
     {
+        readonly string ConnectionString;
+
+        public UsersRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("Stills");
+        }
     }
 }
