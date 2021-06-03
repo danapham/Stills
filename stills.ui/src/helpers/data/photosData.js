0fd0cb9c8020df3ac9655567f0664dedf43ee271
Dana Pham <danapham12@gmail.com>
2021-06-02 20:10:03 -0500

    set up photos data

--- stills.ui/src/helpers/data/photosData.js
+++ stills.ui/src/helpers/data/photosData.js
@@ -1,4 +1,39 @@
-// import axios from 'axios';
-// import { baseUrl } from '../config.json';
+/* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios';
+import { baseUrl } from '../config.json';
 
-// const photosUrl = `${baseUrl}/photos`;
+const photosUrl = `${baseUrl}/photos`;
+
+const addPhoto = (photo) => axios
+    .post(`${photosUrl}`)
+    .catch((err) => console.warn(err));
+
+const getAllPhotos = () => new Promise((resolve, reject) => axios
+    .get(`${photosUrl}`)
+    .then((res) => resolve(res.data))
+    .catch((err) => reject(err))
+);
+
+const getById = (id) => new Promise((resolve, reject) => axios
+.get(`${photosUrl}/${id}`)
+.then((res) => resolve(res.data))
+.catch((err) => reject(err))
+);
+
+const getByFbId = (fbId) => new Promise((resolve, reject) => axios
+.get(`${photosUrl}/${fbId}`)
+.then((res) => resolve(res.data))
+.catch((err) => reject(err))
+);
+
+const getTopVoted = () => new Promise((resolve, reject) => axios
+.get(`${photosUrl}/topVoted`)
+.then((res) => resolve(res.data))
+.catch((err) => reject(err))
+);
+
+const updatePhoto = (id) => axios
+    .put(`${photosUrl}/${id}`)
+    .catch(err => console.warn(err))
+
+export default { addPhoto, getAllPhotos, getById, getByFbId, getTopVoted, updatePhoto }
