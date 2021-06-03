09d6701a7f3d2415340130b56d14e988d2e9512b
Dana Pham <danapham12@gmail.com>
2021-06-02 20:02:55 -0500

    put in liked photos data

--- stills.ui/src/helpers/data/likedPhotosData.js
+++ stills.ui/src/helpers/data/likedPhotosData.js
@@ -1,4 +1,23 @@
-// import axios from 'axios';
-// import { baseUrl } from '../config.json';
+/* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios';
+import { baseUrl } from '../config.json';
 
-// const likedPhotosUrl = `${baseUrl}/likedPhotos`;
+const likedPhotosUrl = `${baseUrl}/likedPhotos`;
+
+const addLikedPhoto = (likedPhoto) => axios
+    .post(`${likedPhotosUrl}`, likedPhoto)
+    .catch((err) => console.warn(err))
+
+const getSingleLikedPhoto = (fbId, photoId) => new Promise((resolve, reject) => axios
+    .get(`${likedPhotosUrl}/${fbId}&${photoId}`)
+    .then((res) => resolve(res.data))
+    .catch((err) => reject(err))
+);
+
+const getByFbId = (fbId) => new Promise((resolve, reject) => axios
+    .get(`${likedPhotosUrl}/${fbId}`)
+    .then((res) => resolve(res))
+    .catch((err) => reject(err))
+)
+
+export default { addLikedPhoto, getSingleLikedPhoto, getByFbId }
