96f4877b0287b002027600908eba22bb9c1babc0
Dana Pham <danapham12@gmail.com>
2021-06-05 10:47:29 -0500

    have form and handle change function

--- stills.ui/src/views/AddP1.js
+++ stills.ui/src/views/AddP1.js
@@ -1,15 +1,63 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+import firebase from 'firebase/app';
 
 export default class AddP1 extends React.Component {
     state = {
+        title: '',
+        category: '',
+        photoUrl: ''
+    }
 
+    handleChange = (e) => {
+        e.preventDefault();
+        if (e.target.id === 'filename') {
+            this.setState({
+                photo: ''
+            });
+            const storageRef = firebase.storage().ref();
+            const imageRef = storageRef.child(`userPhotos/${e.target.files[0].name}-${Date.now()}`);
+            imageRef.put(e.target.files[0]).then((snapshot) => {
+                snapshot.ref.getDownloadURL().then((photoUrl) => {
+                    this.setState({ photoUrl });
+                });
+            });
+        } else {
+            this.setState({
+                [e.target.id]: e.target.value
+            })
+        }
     }
 
     render() {
         return(
-            <>
-            <h1>Add P1</h1>
-            </>
+            <div className="add-page">
+                <Form className="add-form">
+                    <Form.Group controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" onChange={this.handleChange} value={this.state.title} required/>
+                    </Form.Group>
+                    <Form.Group controlId="category">
+                        <Form.Label>Category</Form.Label>
+                        <Form.Control as="select" onChange={this.handleChange} value={this.state.category} required>
+                            <option value="" disabled selected hidden>Select a category</option>
+                            <option value="category">category</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="photoUrl">
+                        <Form.Label>Image Url</Form.Label>
+                        <Form.Control type="url" onChange={this.handleChange} value={this.state.photoUrl} required/>
+                    </Form.Group>
+                    <Form.Group controlId="filename">
+                        <Form.Control type="file" onChange={this.handleChange} />
+                    </Form.Group>
+                    <Form.Group>
+                    <button type="submit">
+                    Submit
+                    </button>
+                    </Form.Group>
+                </Form>
+            </div>
         )
     }
 }
