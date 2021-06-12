59da7a3e911d6759bc1d275c96789f72c08d108d
Dana Pham <danapham12@gmail.com>
2021-06-12 11:28:13 -0500

    have all the top ten chart showing

--- stills.ui/src/views/ChartsP1.js
+++ stills.ui/src/views/ChartsP1.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import photosData from '../helpers/data/photosData';
 
 export default class ChartsP1 extends React.Component {
     state = {
+        photos: []
+    }
 
+    componentDidMount() {
+        photosData.getTopVoted().then((res) => this.setState({ photos: res }))
     }
 
     render() {
         return(
-            <>
-            <h1>Charts P1</h1>
-            </>
+            <div className="charts-p1">
+            <h1 className="hf">Top 10 Photos</h1>
+            <div className="cp1-photos-div">
+                {this.state.photos.map((p) => (
+                    <div className="cp1-photo-div" key={p.id}>
+                        <img src={p.photoUrl} alt={p.title} className="cp1-photo" />
+                    </div>
+                ))}
+            </div>
+            </div>
         )
     }
 }
